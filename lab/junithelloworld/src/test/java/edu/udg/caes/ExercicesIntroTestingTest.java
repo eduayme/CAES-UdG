68610ab9645e78fc1c0f2da88dc06553263e05a8
Edu Aymerich <eduardaymerich@gmail.com>
2018-09-24 18:15:21 +0200

    Changes added to past all tests

--- lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
+++ lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
@@ -9,24 +9,49 @@ public class ExercicesIntroTestingTest {
     @Test
     public void testFindLast(){
         final int result = ExercicesIntroTesting.findLast(new int []{2, 3, 5}, 2);
-        assertEquals(result, -1);
+        assertEquals(result, 0);
     }
 
     @Test
     public void testLastZero(){
         final int result = ExercicesIntroTesting.lastZero(new int []{0, 1, 0});
-        assertEquals(result, 0);
+        assertEquals(result, 2);
     }
 
     @Test
     public void testCountPositive(){
         final int result = ExercicesIntroTesting.countPositive(new int []{-4, 2, 0, 2});
-        assertEquals(result, 3);
+        assertEquals(result, 2);
     }
 
     @Test
     public void testOddOrPos(){
         final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
-        assertEquals(result, 2);
+        assertEquals(result, 3);
+    }
+
+    // New tests
+    @Test
+    public void solve_testFindLast(){
+        final int result = ExercicesIntroTesting.findLast(new int []{2, 3, 5}, 4);
+        assertEquals(result, -1);
+    }
+
+    @Test
+    public void solve_testLastZero(){
+        final int result = ExercicesIntroTesting.lastZero(new int []{0, 1, 2});
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void solve_testCountPositive(){
+        final int result = ExercicesIntroTesting.countPositive(new int []{-4, 2, 1, 2});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void solve_testOddOrPos(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 1, 1, 4});
+        assertEquals(result, 3);
     }
 }
